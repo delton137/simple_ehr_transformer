c9edb49a9bbb395e55bb5585d05a25a548dd4a79
Elton, Daniel Christopher <delton@mgb.org>
2025-08-13 19:07:00 -0400

    test

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@ python test.py \
   --current_data_dir processed_data_aou_pre2023_30000 \
   --future_data_dir processed_data_aou_2023_30000 \
   --output_dir test_results_pre_to_2023 \
-  --num_samples 20 \
+  --num_samples 4 \
   --max_gen_tokens 256 \
   --temperature 1.0 \
   --top_k 50 \
   --top_p 0.9 \
-  --patient_limit 100000
+  --patient_limit 10
