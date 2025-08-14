676d1f16c9853512cd565415c23fa421c1f90fbc
Elton, Daniel Christopher <delton@mgb.org>
2025-08-13 21:36:05 -0400

    test

--- train_and_test.sh
+++ train_and_test.sh
@@ -8,9 +8,14 @@ python test.py \
   --current_data_dir processed_data_aou_pre2023_30000 \
   --future_data_dir processed_data_aou_2023_30000 \
   --output_dir test_results_pre_to_2023 \
-  --num_samples 2 \
-  --max_gen_tokens 1024 \
+  --num_samples 10 \
+  --max_gen_tokens 512\
   --temperature 1.0 \
   --top_k 50 \
   --top_p 0.9 \
-  --patient_limit 10
+  --patient_limit 300 \
+  --targets CONDITION_201826,CONDITION_444070,CONDITION_316139
+
+
+#ITION_201826 diabetes t2d   "CONDITION_444070",	#fatigue  "CONDITION_316139", #heart failure
+  
