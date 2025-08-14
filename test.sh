9e397f5977cb7886d6d8e931b4e58d1cc3438838
Elton, Daniel Christopher <delton@mgb.org>
2025-08-14 10:25:20 -0400

    test

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 python test.py \
-  --debug_samples 2 \
-  --model_path models/aou_pre2023_30000/best_checkpoint.pth \
+  --debug_samples 1 \
+  --model_path models/aou_pre2023_30000/latest_checkpoint.pth \
   --current_data_dir processed_data_aou_pre2023_30000 \
   --future_data_dir processed_data_aou_2023_30000 \
   --output_dir test_results_pre_to_2023 \
-  --num_samples 15 \
+  --num_samples 18 \
   --max_gen_tokens 50000 \
   --temperature 1.0 \
   --top_k 50 \
   --patient_limit 100 \
-  --targets CONDITION_201826,CONDITION_444070,CONDITION_316139
+  --targets CONDITION_201826,CONDITION_444070,CONDITION_316139 \
   --target_names "Type 2 Diabetes","Fatige","Heart Failure"
 
 
