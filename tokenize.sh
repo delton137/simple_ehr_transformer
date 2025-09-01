dec2066ec126ec614beb3d7167c8a3d8801a3179
Elton, Daniel Christopher <delton@mgb.org>
2025-09-01 13:09:10 -0400

    test

--- tokenize.sh
+++ tokenize.sh
@@ -1,10 +1,9 @@
-TAG=aou_2022
-DATA_DIR=/home/jupyter/workspaces/ehrtransformerbaseline/omop_data_2021_2022
+TAG=aou_2022_2023_2024
+DATA_DIR=/home/jupyter/workspaces/ehrtransformerbaseline/omop_data_2021_2022_2023
  
-python data_processor.py --data_path $DATA_DIR --tag $TAG 
+python data_processor.py --data_path $DATA_DIR --tag $TAG -split_by_person --train_frac 0.7 --split_seed 42
 
-
-TAG=aou_2023
-DATA_DIR=/home/jupyter/workspaces/ehrtransformerbaseline/omop_data_2023
+TAG=aou_2024
+DATA_DIR=/home/jupyter/workspaces/ehrtransformerbaseline/omop_data_2024
  
 python data_processor.py --data_path $DATA_DIR --tag $TAG --tokenization_spec processed_data_aou_2022/tokenization.yaml
