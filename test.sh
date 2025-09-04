b15b5f95bdeb110b4afb2e4e54c0af44c87a117d
Daniel Elton <daniel.elton@ccds.io>
2025-09-04 12:46:43 -0400

    t

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
+MODEL_ROOT=/home/jupyter/workspaces/ehrtransformerbaselinecdr8/simple_ehr_transformer/models/aou_2021_2022
+
 python test.py \
   --debug_samples 1 \
-  --model_path models/aou_pre2023_30000/latest_checkpoint.pth \
-  --current_data_dir processed_data_aou_pre2023_30000 \
-  --future_data_dir processed_data_aou_2023_30000 \
-  --output_dir test_results_pre_to_2023 \
+  --model_path $MODEL_ROOT/latest_checkpoint.pth \
+  --current_data_dir processed_data_aou_2021_2022 \
+  --future_data_dir processed_data_aou_2023 \
+  --output_dir test_results_aou_2021_2022 \
   --num_samples 18 \
   --max_gen_tokens 50000 \
   --temperature 1.0 \
