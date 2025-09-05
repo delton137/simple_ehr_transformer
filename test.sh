4f33a7c04270321100bf6b0f22aa24e988d4cfe0
Daniel Elton <daniel.elton@ccds.io>
2025-09-04 20:00:38 -0400

    test

--- test.sh
+++ test.sh
@@ -6,11 +6,11 @@ python test.py \
   --current_data_dir processed_data_aou_2021_2022/train \
   --future_data_dir processed_data_aou_2023 \
   --output_dir test_results_aou_2021_2022 \
-  --num_samples 18 \
+  --num_samples 15 \
   --max_gen_tokens 50000 \
   --temperature 1.0 \
   --top_k 50 \
-  --patient_limit 100 \
+  --patient_limit 300 \
   --targets CONDITION_201826,CONDITION_444070,CONDITION_316139 \
   --target_names "Type 2 Diabetes","Fatige","Heart Failure"
 
